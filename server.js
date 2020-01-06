27de76ec1845a7d3b8aaecf4e41367b705764b73
Stacy Holtz <stacyholtz6@gmail.com>
2020-01-06 10:10:19 -0700

    html layout

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
